e28f65f1b76dda08a46cd8e0b1498765b2b59040
Chirata Andrei <andrei.chirata2006@gmail.com>
2026-09-21 12:49:59 +0300

    finished all methods

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -1,6 +1,7 @@
 #include "../include/Vector.hpp"
 #include <cstring>
 #include <stdexcept>
+#include <algorithm>
 
 namespace custom {
 
@@ -96,4 +97,113 @@ namespace custom {
 
 	const int* Vector::data() const { return m_data; }
 	int* Vector::data() { return m_data; }
+
+	void Vector::reserve(size_t new_capacity) {
+		if (new_capacity <= m_capacity) {
+			return;
+		}
+		int* new_data = new int[new_capacity];
+		if (m_size > 0) {
+			memcpy(new_data, m_data, m_size * sizeof(int));
+		}
+		delete[] m_data;
+		m_data = new_data;
+		m_capacity = new_capacity;
+	}
+
+	void Vector::shrink_to_fit() {
+		if (m_size == m_capacity) {
+			return;
+		}
+
+		if (m_size == 0) {
+			delete[] m_data;
+			m_capacity = 0;
+			return;
+		}
+
+		int* new_data = new int[m_size];
+		std::memcpy(new_data, m_data, m_size * sizeof(int));
+		delete[] m_data;
+		m_data = new_data;
+		m_capacity = m_size;
+	}
+
+	void Vector::resize(size_t new_size, int default_value) {
+		if (m_size == new_size) {
+			return;
+		}
+
+		if (new_size > m_size) {
+			if (new_size > m_capacity) {
+				reserve(new_size);
+			}
+			std::fill(m_data + m_size, m_data + new_size, default_value);
+		}
+		m_size = new_size;
+	}
+
+	void Vector::push_back(int value) {
+		if (m_size == m_capacity) {
+			reserve(2 * m_capacity);
+			m_capacity *= 2;
+		}
+		m_data[m_size++] = value;
+	}
+
+	void Vector::pop_back() {
+		if (empty()) {
+        	throw std::out_of_range("Cannot pop_back from an empty vector");
+    	}
+    	--m_size;
+	}
+
+	void Vector::clear() {
+		m_size = 0;
+	}
+
+	void Vector::insert(size_t index, int value) {
+		if (index > m_size) {
+			throw std::out_of_range("index out of range");
+			return;
+		}
+
+		if (m_size == m_capacity) {
+			reserve((m_capacity == 0) ? 1 : m_capacity * 2);
+		}
+
+		for (size_t i = m_size; i > index; --i) {
+			m_data[i] = m_data[i - 1];
+		}
+		m_data[index] = value;
+		m_size++;
+	}
+
+	void Vector::erase(size_t index) {
+		if (index >= m_size) {
+			throw std::out_of_range("index out of range");
+			return;
+		}
+
+		if (m_size == 0) {
+			return;
+		}
+
+		for (size_t i = m_size - 2; i >= index; i--) {
+			m_data[i] = m_data[i + 1];
+		}
+
+		--m_size;
+	}
+
+	void Vector::swap(Vector& other) {
+		std::swap(m_data, other.m_data);
+		std::swap(m_size, other.m_size);
+		std::swap(m_capacity, other.m_capacity);
+	}
+
+	const int* Vector::begin() const { return m_data; }
+	int* Vector::begin() { return m_data; }
+	const int* Vector::end() const { return m_data + m_size; }
+	int* Vector::end() { return m_data + m_size; }
 }
