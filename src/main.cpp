49714b9365f70e3331c94ee773994d48dbd9041e
Chirata Andrei <andrei.chirata2006@gmail.com>
2026-09-21 14:59:03 +0300

    added CMake

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ int main() {
     vec.push_back(10);
     vec.push_back(20);
     vec.push_back(30);
+	vec.push_back(40);
 
     for (size_t i = 0; i < vec.size(); ++i) {
         std::cout << vec[i] << " ";
